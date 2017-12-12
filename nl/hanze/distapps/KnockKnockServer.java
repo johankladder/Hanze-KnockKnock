3ca7189ce86a20a1c9ee2cf85b31dca44bcf7a7f
Klaas-Anne <klaas.anne@hotmail.com>
2017-12-12 12:27:13 +0100

    multithreading

--- nl/hanze/distapps/KnockKnockServer.java
+++ nl/hanze/distapps/KnockKnockServer.java
@@ -3,28 +3,10 @@ package nl.hanze.distapps;
 import java.net.*;
 import java.io.*;
 
-public class KnockKnockServer {
+public class KnockKnockServer implements Runnable {
 	ServerSocket serverSocket = null;
 	Socket clientSocket = null;
 	KnockKnockProtocol kkp = null;
-	
-	public KnockKnockServer() {
-		makeListener();
-		kkp = new KnockKnockProtocol();
-
-        try {
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
-        }
-        
-        try {
-	       handleRequests();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 	
 	private void handleRequests() throws IOException {
 		String inputLine;
@@ -52,16 +34,45 @@ public class KnockKnockServer {
 	
 	private void makeListener() {
         try {
-            serverSocket = new ServerSocket(Settings.PORT_NUM);
             System.out.println("Server waiting connections on port " +Settings.PORT_NUM);
+            this.serverSocket = new ServerSocket(Settings.PORT_NUM);
         } catch (IOException e) {
             System.err.println("Could not listen on port: " +Settings.PORT_NUM);
             System.exit(1);
         }
 	}
 	
+	public void run() {
+		System.out.println("Client connected in thread: " + Thread.currentThread().getName());
+		kkp = new KnockKnockProtocol();
+	
+        try {
+	       handleRequests();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void accept() {
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+	}
+	
+	public void start () {
+		KnockKnockServer server = new KnockKnockServer();
+		server.makeListener();
+			while(true) {
+				server.accept();
+				Thread thread = new Thread(server);
+				thread.start();
+			}
+	   }
 	
     public static void main(String[] args) throws IOException {
-    	new KnockKnockServer();
+    	new KnockKnockServer().start();
     }
 }
